0a1648f2cd6bb7f062133c6cc95846ac25696bd7
Emanuella Nunes <77542029+EmanuellaNunes@users.noreply.github.com>
2024-04-28 16:19:41 -0400

    14-RW2-29 criacao-modulo-paymentcc

---
+++ src/paymentcc/dto/create-cc-dto.ts
@@ -0,0 +1,21 @@
+import { IsDate, IsNumber, IsString, IsNotEmpty } from "class-validator"
+
+export class CreateCCDto {
+
+    @IsString()
+    @IsNotEmpty()
+    public card_number: string
+
+    @IsDate()
+    @IsNotEmpty()
+    public expiration_date: Date
+
+    @IsNumber()
+    @IsNotEmpty()
+    public security_code: number
+
+    @IsString()
+    @IsNotEmpty()
+    public account_holder: string
+
+}
---
+++ src/paymentcc/entities/paymentcc.entity.ts
@@ -0,0 +1,20 @@
+import { BaseEntity } from "typeorm";
+import { Column, Entity } from "typeorm";
+
+@Entity()
+export class PaymentCC extends BaseEntity{
+    @Column()
+    payment_id: number
+
+    @Column()
+    card_number?: string
+
+    @Column()
+    expiration_date?: Date //Checar herança depois. 
+
+    @Column()
+    security_code?: number
+
+    @Column()
+    account_holder?: string
+}
