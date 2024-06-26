0aa00693c079c04a10f5043bf4c33cfc1c6d9856
Emanuella Nunes <77542029+EmanuellaNunes@users.noreply.github.com>
2024-06-25 23:12:48 -0400

    relatorio teste 1

--- src/voucher/voucher.controller.ts
+++ src/voucher/voucher.controller.ts
@@ -1,18 +1,12 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Post,
-  Query
-} from '@nestjs/common'
+import { Controller, Get, Query, Param, Post, Body, Delete } from '@nestjs/common'
 import { CreateVoucherDto } from './dto/create-voucher.dto'
 import { Voucher } from './entities/voucher.entity'
 import { VoucherService } from './voucher.service'
+import { Pagination } from 'nestjs-typeorm-paginate'
 
 @Controller('voucher')
 export class VoucherController {
-  constructor(private readonly voucherService: VoucherService) { }
+  constructor(private readonly voucherService: VoucherService) {}
 
   @Post()
   create(@Body() createVoucherDto: CreateVoucherDto) {
@@ -23,7 +17,7 @@ export class VoucherController {
   findAll(
     @Query('page') page: number = 1,
     @Query('user_id') user_id: number,
-    @Query('order_by') order_by: keyof Voucher,
+    @Query('order_by') order_by: keyof typeof Voucher,
     @Query('order_by_direction') order_by_direction: 'ASC' | 'DESC',
   ) {
     return this.voucherService.findAll(
@@ -37,8 +31,20 @@ export class VoucherController {
     return this.voucherService.findOne(+id)
   }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: number) {
-  //   return this.voucherService.remove(+id)
-  // }
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.voucherService.remove(+id)
+  }
+
+  @Get('report/by-date')
+  async getVouchersByDate(
+    @Query('date') date: string,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10
+  ): Promise<Pagination<Voucher>> {
+    if (!date) {
+      throw new Error('Date query parameter is required')
+    }
+    return this.voucherService.findVouchersByDate(date, { page, limit })
+  }
 }
